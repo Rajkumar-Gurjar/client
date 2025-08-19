10077d84a4666a6275f1e27a9ae019e4e469db9c
Rajkumar Gurjar <mavairk@gmail.com>
2025-08-20 01:32:23 +0530

    third

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import API from "../services/api";
 
 function Login() {
